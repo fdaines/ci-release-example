4712c71eade0c54bdf6f4ac19f0c08637e7fa3a6
Francisco Daines <fdaines@gmail.com>
2023-03-29 21:56:21 -0300

    Update main.go

--- main.go
+++ main.go
@@ -6,4 +6,6 @@ func main() {
   fmt.Println("Hello World")
   fmt.Println("Hello World xxxxxxx")
   fmt.Println("Hello World xxxxxxx v2")
+  fmt.Println("Hello World xxxxxxx v2")
+  fmt.Println("Hello World xxxxxxx v2")
 }
