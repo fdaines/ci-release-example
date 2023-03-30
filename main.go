6b932b31e551e11a6d5dfb618981f9cd732a89bc
Francisco Daines <fdaines@gmail.com>
2023-03-29 22:34:17 -0300

    blablabla 2

--- main.go
+++ main.go
@@ -4,9 +4,6 @@ import "fmt"
 
 func main() {
   fmt.Println("Hello World")
-  fmt.Println("Hello World xxxxxxx")
-  fmt.Println("Hello World xxxxxxx v2")
-  fmt.Println("Hello World xxxxxxx v2")
   fmt.Println("Bye!!!!!")
   fmt.Println("Bye!!!!!- 2")
 }
